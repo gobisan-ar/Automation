6609e2eea876646f7df48f6383cf2cca525f9e53
Gobisan Ananthanadarajan <88422076+gobisan14@users.noreply.github.com>
2022-07-26 12:37:48 +0530

    Commit Data Entry Automation

---
+++ DataEntry-Automation/main.py
@@ -0,0 +1,65 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+GOOGLE_FROM = "https://forms.gle/5HigMbkm7oAMZzkd9"
+ZILLOW_URL ="https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+ZILLOW_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+
+response = requests.get(url=ZILLOW_URL, headers=ZILLOW_HEADERS)
+response.raise_for_status()
+soup = BeautifulSoup(response.text, 'html.parser')
+
+all_link_elements = soup.select(".list-card-top a")
+all_links = []
+
+for link in all_link_elements:
+    href = link["href"]
+
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+print(all_links)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.text for address in all_address_elements]
+print(all_addresses)
+
+all_price_elements = soup.select(".list-card-price")
+all_prices = [price.text for price in all_price_elements]
+print(all_prices)
+
+price_lbl = "$,/mo"
+
+# format prices
+for i in range(len(all_prices)):
+    for lbl in price_lbl:
+        all_prices[i] = all_prices[i].replace("lbl", "")
+
+# Create spreadsheet using google form
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+for n in range(len(all_links)):
+    driver.get(url=GOOGLE_FROM)
+    print("TEST")
+
+    time.sleep(2)
+
+    address = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    button = driver.find_element(by=By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    button.click()
